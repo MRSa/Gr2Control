c3f541f6bea98bc97ba78bb07cd9c6ec784d28f7
MRSa <mrsa@myad.jp>
2018-08-22 22:46:57 +0900

    余計なimportを削除。

--- app/src/main/java/net/osdn/gokigen/gr2control/camera/utils/SimpleHttpClient.java
+++ app/src/main/java/net/osdn/gokigen/gr2control/camera/utils/SimpleHttpClient.java
@@ -5,9 +5,7 @@ import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
